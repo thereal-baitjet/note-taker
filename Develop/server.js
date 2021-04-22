a80ec92279b915f00f337f6bec501ecae76b748f
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-04-21 22:03:34 -0400

    establishesd routes made css internal

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
 app.listen(PORT, function() {
     console.log(`App listening on PORT: ${PORT}`);
 })
