1a6cca112872f96945c8b70ac89f224e3b023c7f
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-04-24 13:59:01 -0400

    deleted routes file consolidated code in serverjs

--- server.js
+++ server.js
@@ -1,19 +1,81 @@
-const express = require("express");
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+var notes = require("./db/db.json")
 
-const app = express();
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000;
-
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
-
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.listen(PORT, function() {
-    console.log(`App listening on PORT: ${PORT}`);
+currentID = notes.length;
+
+// API Routes
+
+app.get("/api/notes", function (req, res) {
+
+    return res.json(notes);
+});
+
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+
+    newNote["id"] = currentID +1;
+    currentID++;
+    console.log(newNote);
+
+    notes.push(newNote);
+
+    rewriteNotes();
+
+    return res.status(200).end();
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    res.send('Got a DELETE request at /api/notes/:id')
+
+    var id = req.params.id;
+
+    var idLess = notes.filter(function (less) {
+        return less.id < id;
+    });
+
+    var idGreater = notes.filter(function (greater) {
+        return greater.id > id;
+    });
+
+    notes = idLess.concat(idGreater);
+
+    rewriteNotes();
 })
 
-// created an express server that listens on port 3000
+// HTML Routes
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// Listen
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
+// Functions
+
+function rewriteNotes() {
+    fs.writeFile("db/db.json", JSON.stringify(notes), function (err) {
+        if (err) {
+            console.log("error")
+            return console.log(err);
+        }
+
+        console.log("note taken");
+    });
+}
